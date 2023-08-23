b8a13522e0cc6768a88773f98ef3d75ff516e321
@muskansoni482 <muskan.soni@appinventiv.com>
2023-08-23 13:24:38 +0530

    Eager issue on react router dom  sharing

--- recipies/src/App.js
+++ recipies/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Recipes from "./Recipes";
+import Recipe from "./Recipe";
 
 function App() {
   return (
     <div>
-    Recipes
-  </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Recipes />} />
+          <Route path="/recipes/:slug" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
